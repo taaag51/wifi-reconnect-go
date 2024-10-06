04daf1868e58d8e4a003c618789f8fa106536815
taaag51 <inat0510@gmail.com>
2024-10-06 16:28:25 +0900

    とりあえず作成

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/taaag51/wifi-reconnect-go
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"time"
+)
+
+func log(message string) {
+	fmt.Printf("%s - %s\n", time.Now().Format("2006-01-02 15:04:05"), message)
+}
+
+func checkInternet() bool {
+	cmd := exec.Command("ping", "-c", "1", "-w", "3", "google.com")
+	err := cmd.Run()
+	return err == nil
+}
+
+func restartWiFi() {
+	exec.Command("networksetup", "-setairportpower", "en0", "off").Run()
+	time.Sleep(5 * time.Second)
+	exec.Command("networksetup", "-setairportpower", "en0", "on").Run()
+
+}
+
+func main() {
+	log("Wi-Fi監視スクリプトを開始しました")
+
+	for {
+		if !checkInternet() {
+			log("インターネット接続が切断されました、Wi-Fiを再起動します")
+
+			restartWiFi()
+			log("Wi-Fiを再起動しました、再接続を確認中...")
+
+			for i := 1; i < 5; i++ {
+				time.Sleep(10 * time.Second)
+				if checkInternet() {
+					log("インターネット接続が復旧しました")
+					break
+				} else if i == 5 {
+					log("インターネット接続の復旧に失敗しました、次の確認まで待機します")
+				} else {
+					log(fmt.Sprintf("再接続を確認中... (試行%d/5)", i))
+				}
+			}
+		} else {
+			log("インターネット接続は正常です")
+		}
+		time.Sleep(15 * time.Second)
+	}
+}
